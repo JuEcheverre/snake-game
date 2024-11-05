7256d0a4faac12cea1e72bb27b1a1bd5e543367b
Juliana <juliana@Daniels-MacBook-Pro.local>
2024-11-05 17:55:09 -0300

    Criação da funcionalidade de GAME OVER

--- cobrinha.py
+++ cobrinha.py
@@ -28,6 +28,7 @@ comida_y = round(random.randrange(0, altura - tamanho_cobra) / 10.0) * 10.0
 # Loop do jogo
 relogio = pygame.time.Clock()
 jogo_ativo = True
+game_over = False
 while jogo_ativo:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
@@ -50,6 +51,9 @@ while jogo_ativo:
 
     # Atualização da posição e do corpo da cobra
     cabeca_cobra = [x, y]
+    if(cabeca_cobra in corpo_cobra and (dx != 0 or dy != 0)):
+        print("Bateu")
+        game_over = True
     corpo_cobra.append(cabeca_cobra)
     if len(corpo_cobra) > comprimento_cobra:
         del corpo_cobra[0]
@@ -62,20 +66,35 @@ while jogo_ativo:
         comida_x = round(random.randrange(0, largura - tamanho_cobra) / 10.0) * 10.0
         comida_y = round(random.randrange(0, altura - tamanho_cobra) / 10.0) * 10.0
     
-    if x <= 0:
+    if x < 0:
         x = largura
-    elif x >= largura:
+    elif x > largura:
         x = 0
-    if y <= 0:
+    if y < 0:
         y = altura
-    elif y >= altura:
+    elif y > altura:
         y = 0
-    if comprimento_cobra >= 10:
-        jogo_ativo = False
+
+    while game_over:
+        tela.fill(bk_tela)
+        fonte = pygame.font.Font(None, 50)
+        texto = fonte.render("Game Over", True, (255, 0, 0))  # Cor vermelha para destaque
+        tela.blit(texto, (largura // 2 - 100, altura // 2 - 25))  # Centraliza o texto
+        pygame.display.flip()
+        x, y = largura // 2, altura // 2
+        dx, dy = 0, 0
+        corpo_cobra = []
+        comprimento_cobra = 1
+        comida_x = round(random.randrange(0, largura - tamanho_cobra) / 10.0) * 10.0
+        comida_y = round(random.randrange(0, altura - tamanho_cobra) / 10.0) * 10.0
+        pygame.time.delay(2000)  # Pausa por 2 segundos antes de fechar
+        game_over = False
 
     pygame.display.update()
     relogio.tick(velocidade_cobra)
 
 pygame.quit()
 
-
+#TODO: Consertar surgimento da borda superior e esquerda
+#TODO: Consertar comportamento da marcha re
+#TODO: Marcha re não deve dar gameover
